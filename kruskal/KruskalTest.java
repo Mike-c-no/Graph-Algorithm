15378cff835c4ab289781db53e07e947de2f44d4
tinsaeabebe <tabebe554@gmail.com>
2022-06-20 20:23:48 +0300

    kruskal Implementation On Graph Algorithm

---
+++ kruskal/KruskalTest.java
@@ -0,0 +1,38 @@
+
+package kruskal;
+
+/**
+ *
+ * @author Tinsa
+ */
+public class KruskalTest {
+     public static void main(String[] args) {
+        int V = 4;    
+        int E = 5;       
+            Kruskal graph = new Kruskal(V, E); 
+         
+                graph.edge[0].src = 0; 
+                graph.edge[0].dest = 1; 
+                graph.edge[0].weight = 10; 
+        
+                graph.edge[1].src = 0; 
+                graph.edge[1].dest = 2; 
+                graph.edge[1].weight = 6; 
+
+         
+                graph.edge[2].src = 0; 
+                graph.edge[2].dest = 3; 
+                graph.edge[2].weight = 5;
+  
+                graph.edge[3].src = 1; 
+                graph.edge[3].dest = 3; 
+                graph.edge[3].weight = 15;  
+         
+                graph.edge[4].src = 2; 
+                graph.edge[4].dest = 3; 
+                graph.edge[4].weight = 4;  
+        
+                graph.KruskalMST(); 
+    }
+    
+}
